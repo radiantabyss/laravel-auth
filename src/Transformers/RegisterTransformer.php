ce5acca8efda040b40cdf91441a8b145775303aa
Robert Buchi <robert@planetweb.ro>
2022-07-12 10:08:47 +0300

    default user type

--- src/Transformers/RegisterTransformer.php
+++ src/Transformers/RegisterTransformer.php
@@ -5,12 +5,9 @@ class RegisterTransformer
 {
     public static function run($data) {
         $data['uuid'] = \Str::uuid();
+        $data['type'] = config('ra-auth.default_user_type');
         $data['password'] = \Hash::make($data['password']);
         $data['activation_code'] = \Str::random(10);
-
-        if ( !isset($data['type']) ) {
-            $data['type'] = config('ra-auth.default_user_type');
-        }
 
         return $data;
     }
